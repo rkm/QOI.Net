ae2c80dbab956c24273b94c23e2a241365bbc85b
Ruairidh MacLeod <ruairidh@rkm.dev>
2021-12-27 16:32:35 +0000

    add test image and conversion scripts

---
+++ tools/binToJpeg.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import argparse
+import io
+import json
+import os
+
+from PIL import Image
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input")
+    parser.add_argument("width")
+    parser.add_argument("height")
+    args = parser.parse_args()
+
+
+    with open(args.input, "rb") as f:
+        as_bytes = f.read()
+
+    base_name = os.path.splitext(args.input)[0]
+    with open(f"{base_name}.json") as f:
+        info = json.load(f)
+
+    img = Image.frombytes("RGBA", (int(args.width), int(args.height)), as_bytes)
+    img.convert("RGB").save(f"{base_name}-out.jpeg", **info)
+
+    return 0
+
+if __name__ == "__main__":
+	raise SystemExit(main())
+
---
+++ tools/imgToBin.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+import sys
+
+from PIL import Image
+from PIL import ImageChops
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input")
+    args = parser.parse_args()
+
+    img = Image.open(args.input)
+
+    base_name = os.path.splitext(args.input)[0]
+
+    print(f"size: {img.size}")
+    print(f"info: {img.info}")
+
+    with open(f"{base_name}.json", "w") as f:
+        json.dump( img.info, f)
+        f.write("\n")
+
+    return 0
+
+    as_bytes = img.convert("RGBA").tobytes()
+
+    outname = os.path.splitext(args.input)[0] + ".bin"
+    print(f"Writing to {outname}")
+    with open(outname, "wb") as f:
+        f.write(as_bytes)
+
+    return 0
+
+if __name__ == "__main__":
+	raise SystemExit(main())
